a2c7f5ea07e8ec57a52b4f54830dd448371b5d42
slhcnyce <salihcanyce@gmail.com>
2022-02-01 23:26:43 +0300

    Temperature endpoint created, city and tempinfo variables added.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const request = require("request")
 const app = express()
+PORT = 8080
 
 app.get('/', function (req, res) {
   res.send({"firstname": "Salihcan", "lastname": "Yüce"})
 })
 
-app.listen(3000)
+app.get('/temperature', function (req, res) {
+
+  var tempinfo = "";
+  requestedcity = req.query.city
+
+  
+  })
+app.listen(PORT,
+  () => console.log("http://localhost:8080 Online!")
+  )
